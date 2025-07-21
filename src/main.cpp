a414971ce4e26159bbfb54678d39f19d55e51210
Glas42 <145870870+OleFranz@users.noreply.github.com>
2025-07-21 02:36:50 +0200

    add code to identify fragments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <windivert.h>
 
 #include <unordered_map>
+#include <vector>
 #include <windows.h>
 #include <cstdint>
 #include <psapi.h>
@@ -22,7 +23,7 @@ struct FlowKey {
     uint16_t src_port;
     uint32_t dst_addr;
     uint16_t dst_port;
-    uint8_t  proto;
+    uint8_t proto;
 
     bool operator==(FlowKey const& o) const {
         return src_addr==o.src_addr
@@ -44,8 +45,34 @@ struct FlowKeyHash {
 };
 
 
+struct FragmentKey {
+    uint32_t src;
+    uint32_t dst;
+    uint16_t id;
+    uint8_t proto;
+
+    bool operator==(FragmentKey const& o) const {
+        return src == o.src && dst == o.dst
+            && id == o.id && proto == o.proto;
+    }
+};
+
+struct FragmentKeyHash {
+    size_t operator()(FragmentKey const& k) const noexcept {
+        return (size_t)k.proto
+             ^ ((size_t)k.src << 1)
+             ^ ((size_t)k.dst << 2)
+             ^ ((size_t)k.id << 3);
+    }
+};
+
+
 static std::unordered_map<FlowKey, DWORD, FlowKeyHash> flow_to_pid;
 static std::mutex map_mutex;
+
+static std::unordered_map<FragmentKey, std::vector<uint8_t>, FragmentKeyHash> frag_buf;
+static std::unordered_map<FragmentKey, size_t, FragmentKeyHash> frag_len;
+static std::mutex frag_mutex;
 
 
 void FlowLayerListener() {
@@ -62,27 +89,21 @@ void FlowLayerListener() {
         return;
     }
 
-    while (true)
-    {
+    while (true) {
         if (!WinDivertRecv(flow_handle, nullptr, 0, nullptr, &addr)) {
             fprintf(stderr, "WinDivertRecv(flow) failed: %s\n", recv_error_to_string(GetLastError()).c_str());
             continue;
         }
 
-        FlowKey key;
-        key.src_addr = addr.Flow.LocalAddr[0];
-        key.src_port = (uint16_t)addr.Flow.LocalPort;
-        key.dst_addr = addr.Flow.RemoteAddr[0];
-        key.dst_port = (uint16_t)addr.Flow.RemotePort;
-        key.proto    = (uint8_t)addr.Flow.Protocol;
+        FlowKey flow_key;
+        flow_key.src_addr = addr.Flow.LocalAddr[0];
+        flow_key.src_port = (uint16_t)addr.Flow.LocalPort;
+        flow_key.dst_addr = addr.Flow.RemoteAddr[0];
+        flow_key.dst_port = (uint16_t)addr.Flow.RemotePort;
+        flow_key.proto = (uint8_t)addr.Flow.Protocol;
 
         std::lock_guard<std::mutex> lk(map_mutex);
-        if (true) {
-            flow_to_pid[key] = addr.Flow.ProcessId;
-        }
-        else {
-            flow_to_pid.erase(key);
-        }
+        flow_to_pid[flow_key] = addr.Flow.ProcessId;
     }
 
     WinDivertClose(flow_handle);
@@ -111,19 +132,119 @@ void NetworkLayerListener() {
             continue;
         }
 
-        PWINDIVERT_IPHDR   iphdr = nullptr;
+        PWINDIVERT_IPHDR iphdr = nullptr;
         PWINDIVERT_IPV6HDR ipv6hdr = nullptr;
-        PWINDIVERT_TCPHDR  tcphdr = nullptr;
-        PWINDIVERT_UDPHDR  udphdr = nullptr;
 
+        WinDivertHelperParsePacket(
+            packet, packet_len,
+            &iphdr, &ipv6hdr,
+            nullptr, nullptr, nullptr,
+            nullptr, nullptr,
+            nullptr, nullptr,
+            nullptr, nullptr
+        );
+
+        bool is_fragment = false;
+        FragmentKey fragment_key{};
+        UINT16 offset8 = 0;
+        bool more_fragments = false;
+        uint8_t* payload_ptr = nullptr;
+        UINT payload_len = 0;
+
+        if (iphdr) {
+            UINT16 fragment_offset = WINDIVERT_IPHDR_GET_FRAGOFF(iphdr); // offset
+            more_fragments = WINDIVERT_IPHDR_GET_MF(iphdr); // if true, more fragments expected
+            is_fragment = (fragment_offset != 0) || more_fragments;
+            if (is_fragment) {
+                offset8 = fragment_offset;
+                payload_ptr = (uint8_t*)iphdr + (iphdr->HdrLength * 4);
+                payload_len = WinDivertHelperNtohs(iphdr->Length) - (iphdr->HdrLength * 4);
+                fragment_key.src = WinDivertHelperNtohl(iphdr->SrcAddr);
+                fragment_key.dst = WinDivertHelperNtohl(iphdr->DstAddr);
+                fragment_key.id = WinDivertHelperNtohs(iphdr->Id);
+                fragment_key.proto = iphdr->Protocol;
+                std::lock_guard<std::mutex> lk(frag_mutex);
+                auto &buf = frag_buf[fragment_key];
+                auto &tot = frag_len[fragment_key];
+                size_t needed = (offset8 * 8) + payload_len;
+                if (buf.size() < needed) buf.resize(needed);
+                memcpy(buf.data() + offset8 * 8, payload_ptr, payload_len);
+                if (tot < needed) tot = needed;
+
+                if (!more_fragments) {
+                    UINT hdrLen = iphdr->HdrLength * 4;
+                    UINT newLen = hdrLen + (UINT)tot;
+                    std::vector<uint8_t> newPkt(newLen);
+                    memcpy(newPkt.data(), packet, hdrLen);
+                    auto* newIph = (PWINDIVERT_IPHDR)newPkt.data();
+                    newIph->Length = WinDivertHelperHtons((UINT16)newLen);
+                    WINDIVERT_IPHDR_SET_FRAGOFF(newIph, 0);
+                    WINDIVERT_IPHDR_SET_MF(newIph, 0);
+                    memcpy(newPkt.data() + hdrLen, buf.data(), tot);
+                    PWINDIVERT_TCPHDR tcphdr = nullptr;
+                    PWINDIVERT_UDPHDR udphdr = nullptr;
+                    PWINDIVERT_ICMPHDR icmphdr = nullptr;
+                    PWINDIVERT_ICMPV6HDR icmpv6hdr = nullptr;
+
+                    WinDivertHelperParsePacket(
+                        newPkt.data(),
+                        newLen,
+                        nullptr,
+                        nullptr,
+                        nullptr,
+                        &icmphdr,
+                        &icmpv6hdr,
+                        &tcphdr,
+                        &udphdr,
+                        nullptr,
+                        nullptr,
+                        nullptr,
+                        nullptr
+                    );
+
+                    FlowKey flow_key{};
+                    flow_key.src_addr = fragment_key.src;
+                    flow_key.dst_addr = fragment_key.dst;
+                    flow_key.proto = fragment_key.proto;
+                    if (tcphdr) {
+                        flow_key.src_port = WinDivertHelperNtohs(tcphdr->SrcPort);
+                        flow_key.dst_port = WinDivertHelperNtohs(tcphdr->DstPort);
+                    } else if (udphdr) {
+                        flow_key.src_port = WinDivertHelperNtohs(udphdr->SrcPort);
+                        flow_key.dst_port = WinDivertHelperNtohs(udphdr->DstPort);
+                    } else {
+                        flow_key.src_port = 0;
+                        flow_key.dst_port = 0;
+                    }
+                    DWORD pid = -1;
+                    {
+                        std::lock_guard<std::mutex> lk2(map_mutex);
+                        auto it = flow_to_pid.find(flow_key);
+                        if (it != flow_to_pid.end()) {
+                            pid = it->second;
+                        }
+                    }
+
+
+                    frag_buf.erase(fragment_key);
+                    frag_len.erase(fragment_key);
+                }
+                continue;
+            }
+        }
+
+        PWINDIVERT_TCPHDR tcphdr = nullptr;
+        PWINDIVERT_UDPHDR udphdr = nullptr;
+        PWINDIVERT_ICMPHDR icmphdr = nullptr;
+        PWINDIVERT_ICMPV6HDR icmpv6hdr = nullptr;
         WinDivertHelperParsePacket(
             packet,
             packet_len,
             &iphdr,
             &ipv6hdr,
             nullptr,
-            nullptr,
-            nullptr,
+            &icmphdr,
+            &icmpv6hdr,
             &tcphdr,
             &udphdr,
             nullptr,
@@ -132,52 +253,37 @@ void NetworkLayerListener() {
             nullptr
         );
 
-        if (!iphdr && !ipv6hdr) {
-            // not IPv4/IPv6
-            if (!WinDivertSend(network_handle, packet, packet_len, nullptr, &addr)) {
-                fprintf(stderr, "WinDivertSend(network) failed: %s\n", send_error_to_string(GetLastError()).c_str());
-            }
-            continue;
-        }
 
-        FlowKey key;
+        FlowKey flow_key;
         if (iphdr) {
-            key.src_addr = WinDivertHelperNtohl(iphdr->SrcAddr);
-            key.dst_addr = WinDivertHelperNtohl(iphdr->DstAddr);
-            key.proto = iphdr->Protocol;
+            flow_key.src_addr = WinDivertHelperNtohl(iphdr->SrcAddr);
+            flow_key.dst_addr = WinDivertHelperNtohl(iphdr->DstAddr);
+            flow_key.proto = iphdr->Protocol;
         } else {
-            key.src_addr = WinDivertHelperNtohl(ipv6hdr->SrcAddr[3]);
-            key.dst_addr = WinDivertHelperNtohl(ipv6hdr->DstAddr[3]);
-            key.proto = ipv6hdr->NextHdr;
+            flow_key.src_addr = WinDivertHelperNtohl(ipv6hdr->SrcAddr[3]);
+            flow_key.dst_addr = WinDivertHelperNtohl(ipv6hdr->DstAddr[3]);
+            flow_key.proto = ipv6hdr->NextHdr;
         }
 
-        // Get ports if TCP/UDP, otherwise use 0
         if (tcphdr) {
-            key.src_port = WinDivertHelperNtohs(tcphdr->SrcPort);
-            key.dst_port = WinDivertHelperNtohs(tcphdr->DstPort);
+            flow_key.src_port = WinDivertHelperNtohs(tcphdr->SrcPort);
+            flow_key.dst_port = WinDivertHelperNtohs(tcphdr->DstPort);
         } else if (udphdr) {
-            key.src_port = WinDivertHelperNtohs(udphdr->SrcPort);
-            key.dst_port = WinDivertHelperNtohs(udphdr->DstPort);
+            flow_key.src_port = WinDivertHelperNtohs(udphdr->SrcPort);
+            flow_key.dst_port = WinDivertHelperNtohs(udphdr->DstPort);
         } else {
-            key.src_port = 0;
-            key.dst_port = 0;
+            flow_key.src_port = 0;
+            flow_key.dst_port = 0;
         }
 
-        const char* proto_str;
-        switch(key.proto) {
-            case IPPROTO_TCP: proto_str = "TCP"; break;
-            case IPPROTO_UDP: proto_str = "UDP"; break;
-            case IPPROTO_ICMP: proto_str = "ICMP"; break;
-            case IPPROTO_ICMPV6: proto_str = "ICMPV6"; break;
-            default: proto_str = "UNKNOWN"; break;
-        }
 
         DWORD pid = -1;
         {
             std::lock_guard<std::mutex> lk(map_mutex);
-            auto it = flow_to_pid.find(key);
-            if (it != flow_to_pid.end())
+            auto it = flow_to_pid.find(flow_key);
+            if (it != flow_to_pid.end()) {
                 pid = it->second;
+            }
         }
 
 
@@ -198,8 +304,17 @@ void NetworkLayerListener() {
             }
         }
 
+        const char* proto_str;
+        switch (flow_key.proto) {
+            case IPPROTO_TCP: proto_str = "TCP"; break;
+            case IPPROTO_UDP: proto_str = "UDP"; break;
+            case IPPROTO_ICMP: proto_str = "ICMP"; break;
+            case IPPROTO_ICMPV6: proto_str = "ICMPV6"; break;
+            default: proto_str = "UNKNOWN"; break;
+        }
+
         printf(
-            "[%s-%s] %s (%d) %d bytes\n",
+            "[%s-%s] %s (%d) %u bytes\n",
             ip_ver,
             proto_str,
             executable,
@@ -216,8 +331,7 @@ void NetworkLayerListener() {
     WinDivertClose(network_handle);
 }
 
-int main()
-{
+int main() {
     std::thread sockThr(FlowLayerListener);
     std::thread netThr(NetworkLayerListener);
 
